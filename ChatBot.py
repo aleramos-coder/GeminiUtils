212131ed571d8a6cc41785233927e764584fb948
Alex Ramos <JRAMOSNETO01@mynbcc.ca>
2024-09-23 11:41:42 -0300

    Initial classes setup

--- ChatBot.py
+++ ChatBot.py
@@ -1,10 +1,19 @@
 from abc import ABC, abstractmethod
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+from sympy.physics.units import temperature
+
+from chatbot.apiKey import returnKey
+
 
 class CommandDTO:
-    def __init__(self, role: str, tone: str, prompt: str):
+    def __init__(self, role: str, tone: str, prompt: str, limit = 150, temperature = 1):
         self.__role = role
         self.__tone = tone
         self.__prompt = prompt
+        self.__limit = limit
+        self.__temperature = temperature
 
     @property
     def role(self) -> str:
@@ -18,14 +27,50 @@ class CommandDTO:
     def prompt(self) -> str:
         return self.__prompt
 
+    @property
+    def limit(self) -> int:
+        return self.__limit
+    @property
+    def temperature(self) -> float:
+        return self.__temperature
+
+
 class ChatBot(ABC):
+    @abstractmethod
+    def getKey(self)->str:
+        pass
     @abstractmethod
     def create(self) -> 'ChatBot':
         pass
 
+    @abstractmethod
+    def generateText(self, command:CommandDTO)->str:
+        pass
+
 class Gemini(ChatBot):
+    def getKey(self)->str:
+        load_dotenv("API_KEY.env")
+        return os.getenv("API_KEY") or returnKey()
+
     def create(self) -> 'Gemini':
         return Gemini()
+
+    def generateText(self, command:CommandDTO) -> str:
+        genai.configure(api_key=self.getKey())
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        try:
+            response = model.generate_content(f'''
+            imagine you are a {command.role},
+            using this tone {command.tone}
+            characters limit {command.limit}
+            do this: {command.prompt}
+            ''', generation_config = genai.GenerationConfig(temperature = command.temperature))
+            return response.text
+
+        except Exception as e:
+            print(e)
+            raise e
+
 
 class BotFactory:
     def getBot(self, type: str = "Gemini") -> ChatBot:
@@ -34,5 +79,5 @@ class BotFactory:
         else:
             raise Exception("Bot implementation not found")
 
-bot = BotFactory().getBot("Gemini")
-print("Bot: ", bot.__class__.__name__)
+
+
