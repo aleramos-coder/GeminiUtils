3ebbdf80e584ed34ba1265f63ffa84209b6fc6b4
Alex Ramos <JRAMOSNETO01@mynbcc.ca>
2024-09-27 09:58:49 -0300

    modified Command class to accept history argument and persist short term context

--- CommandDTO.py
+++ CommandDTO.py
@@ -1,11 +1,12 @@
 class CommandDTO:
-    def __init__(self, role: str="yourself", tone: str="neutral", prompt: str="random comment", limit = 150, temperature = 1, imgURL = None):
+    def __init__(self, role: str="yourself", tone: str="neutral", prompt: str="random comment", limit = 150, temperature = 1, imgURL = None, history: list = None):
         self.__role = role
         self.__tone = tone
         self.__prompt = prompt
         self.__limit = limit
         self.__temperature = temperature
         self.__imgURL = imgURL
+        self.__history = history
 
     @property
     def role(self) -> str:
@@ -19,6 +20,10 @@ class CommandDTO:
     def prompt(self) -> str:
         return self.__prompt
 
+    @prompt.setter
+    def prompt(self, value: str):
+        self.__prompt = value
+
     @property
     def limit(self) -> int:
         return self.__limit
@@ -29,3 +34,7 @@ class CommandDTO:
     @property
     def imgURL(self) -> str:
         return self.__imgURL
+
+    @property
+    def history(self) -> list:
+        return self.__history
